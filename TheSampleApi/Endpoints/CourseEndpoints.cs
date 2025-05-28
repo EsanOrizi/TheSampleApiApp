6fbaf287199a5b1c13feb29d67c9d9f50865019d
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2025-05-28 18:04:39 +0100

    Data filtering added

--- TheSampleApi/Endpoints/CourseEndpoints.cs
+++ TheSampleApi/Endpoints/CourseEndpoints.cs
@@ -11,14 +11,41 @@ namespace TheSampleApi.Endpoints
             app.MapGet("/courses/{id}", LoadCourseById);
         }
 
-        private static IResult LoadAllCourses(CourseData data)
+        private static IResult LoadAllCourses(
+            CourseData data, 
+            string? courseType,
+            string? search)
         {
-            return Results.Ok(data.Courses);
+
+            var output = data.Courses;
+
+            if (string.IsNullOrWhiteSpace(courseType) == false)
+            {
+                output.RemoveAll(x => string.Compare(
+                    x.CourseType,
+                    courseType,
+                    StringComparison.OrdinalIgnoreCase) != 0);
+            }
+
+            if (string.IsNullOrWhiteSpace(search) == false)
+            {
+                output.RemoveAll(x => !x.CourseName.Contains(search, StringComparison.OrdinalIgnoreCase) &&
+                !x.ShortDescription.Contains(search, StringComparison.OrdinalIgnoreCase)
+                );
+            }
+
+            return Results.Ok(output);
         }
 
         private static IResult LoadCourseById(CourseData data, int id)
         {
-            return Results.Ok(data.Courses.SingleOrDefault(x => x.Id == id));
+            var output = data.Courses.SingleOrDefault(x => x.Id == id);
+
+            if (output is null)
+            {
+                return Results.NotFound();
+            }
+            return Results.Ok(output);
         }
     }
 

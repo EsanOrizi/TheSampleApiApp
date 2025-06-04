1049686be25eb55f59cbcd4ca724a7fc7afd5da8
Esan Alorizi <62729091+EsanOrizi@users.noreply.github.com>
2025-06-04 16:13:22 +0100

    adding healthchecks

---
+++ TheSampleApi/HealthChecks/UnhealthyHealthCheck.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace TheSampleApi.HealthChecks;
+
+public class UnhealthyHealthCheck : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context,
+        CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(
+            HealthCheckResult.Unhealthy("The is a test unhealthy service."));
+    }
+}
+{
+}
